23d27538892c32af232c087753cf3cd557168ab7
OlegKhiretdinov <oleg@oleg.win>
2022-03-07 15:53:52 +0300

    F-03 добавил данные в OrderInfo для первого шага

--- src/components/OrderInfo/OrderInfo.jsx
+++ src/components/OrderInfo/OrderInfo.jsx
@@ -1,14 +1,20 @@
+import { useSelector } from "react-redux"
 import LinkButton from "../LinkButton/LinkButton"
 import cls from "./OrderInfo.module.scss"
 
 const OrderInfo = () => {
+  const location = useSelector((state) => state.location)
+  const stepOneString = location.point?.address
+    ? `${location.city.name}, ${location.point?.address}`
+    : ""
+
   return (
     <div className={cls.wrapper}>
       <h3 className={cls.title}>Ваш Заказ:</h3>
       <div className={cls.row}>
         <div className={cls.label}>Пункт выдачи</div>
         <div className={cls.space} />
-        <div className={cls.value}>Ульяновск, Нариманова 42</div>
+        <div className={cls.value}>{stepOneString}</div>
       </div>
       <div className={cls.price}>
         <b>Цена:</b> от 8 000 до 12 000 ₽

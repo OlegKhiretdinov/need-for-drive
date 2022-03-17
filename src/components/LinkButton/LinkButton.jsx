4ce8d4cea01d9f512810a39e07d86129bd33427e
OlegKhiretdinov <oleg@oleg.win>
2022-03-17 19:46:07 +0300

    F-01 исправил названия классов

--- src/components/LinkButton/LinkButton.jsx
+++ src/components/LinkButton/LinkButton.jsx
@@ -3,7 +3,7 @@ import cls from "./LinkButton.module.scss"
 
 const LinkButton = ({ to, text, isLoading, className }) => {
   return (
-    <Link className={`${cls.Link} ${className}`} to={to}>
+    <Link className={`${cls.link} ${className}`} to={to}>
       {isLoading ? null : text}
     </Link>
   )
